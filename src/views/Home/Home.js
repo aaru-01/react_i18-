dea7d9e08768ff91b5f88f81085711b6a69c80b5
aaru01 <aartimedhe16601@gmail.com>
2023-11-24 10:11:24 +0530

    Initial commit

---
+++ src/views/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./Home.css"
+function Home() {
+  return (
+    <div>
+      <div>
+     <h1>Welcome.!</h1>
+     <p>We hope you enjoy learning React.</p>
+     <h3>Have a great day!</h3>
+    </div>
+    </div>
+  )
+}
+
+export default Home
+
